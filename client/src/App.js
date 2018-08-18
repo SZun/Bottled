21a3573775822849967cdc48758ee5ffb9e5049f
Sam G. Zun <zunsamuel97@gmail.com>
2018-08-18 07:15:14 -0500

    Added Lazy Loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Homepage from './containers/homepage.jsge';
-import SignIn from './containers/SignIn';
+import asyncComponent from './utils/asyncComponent';
+import Layout from './containers/Layout';
+// import PrivateRoute from './components/PrivateRoute'
+
+// Lazy Loading Components
+const Homepage = asyncComponent(() => import('./containers/Homepage'));
+const SignUp = asyncComponent(() => import('./containers/SignIn'));
+const Login = asyncComponent(() => import('./containers/Login'));
 
 const App = () => {
   return (
-    <div>
+    <Layout>
       <Route exact path="/" component={Homepage} />
-      <Route exact path="/sign-in" component={SignIn} />
-    </div>
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/login" component={Login} />
+    </Layout>
   );
 };
 
