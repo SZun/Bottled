d4587a78a5601fa406897774fa4f7124fa1a5ed1
Sam G. Zun <zunsamuel97@gmail.com>
2018-08-24 23:08:51 -0500

    added cart check for logged in user

--- client/src/containers/Layout/Layout.js
+++ client/src/containers/Layout/Layout.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setCurrentUser, logoutUser } from '../../store/actions/authActions';
+import { fetchNotPurchased } from '../../store/actions/orderActions';
 import { withRouter } from 'react-router-dom';
 import store from '../../store';
 import jwt_decode from 'jwt-decode';
@@ -9,46 +10,52 @@ import PropTypes from 'prop-types';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
 
-// Check for toekn
-if (localStorage.jwtToken) {
-  // Set auth token header auth
-  setAuthToken(localStorage.jwtToken);
-  // Decode tokent and get user info and exp
-  const decoded = jwt_decode(localStorage.jwtToken);
-  // Set user and isAuthenticated
-  store.dispatch(setCurrentUser(decoded));
+class Layout extends Component {
+  render() {
+    // Check for toekn
+    if (localStorage.jwtToken) {
+      // Set auth token header auth
+      setAuthToken(localStorage.jwtToken);
+      // Decode tokent and get user info and exp
+      const decoded = jwt_decode(localStorage.jwtToken);
+      // Set user and isAuthenticated
+      store.dispatch(setCurrentUser(decoded));
 
-  // Check for expired token
-  const currentTime = Date.now() / 1000;
-  if (decoded.exp < currentTime) {
-    // Clear current token
-    localStorage.removeItem('jwtToken');
-    // Logout user
-    store.dispatch(logoutUser(this.props.history));
+      this.props.fetchNotPurchased();
+
+      // Check for expired token
+      const currentTime = Date.now() / 1000;
+      if (decoded.exp < currentTime) {
+        // Clear current token
+        localStorage.removeItem('jwtToken');
+        // Logout user
+        store.dispatch(logoutUser(this.props.history));
+      }
+    }
+    return (
+      <div>
+        <header>
+          <nav>
+            <Navbar />
+          </nav>
+        </header>
+        <main>{this.props.children}</main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    );
   }
 }
-
-const Layout = ({ children }) => (
-  <div>
-    <header>
-      <nav>
-        <Navbar />
-      </nav>
-    </header>
-    <main>{children}</main>
-    <footer>
-      <Footer />
-    </footer>
-  </div>
-);
 
 Layout.propTypes = {
   setAuthToken: PropTypes.func.isRequired,
   logoutUser: PropTypes.func.isRequired,
-  setCurrentUser: PropTypes.func.isRequired
+  setCurrentUser: PropTypes.func.isRequired,
+  fetchNotPurchased: PropTypes.func.isRequired
 };
 
 export default connect(
   null,
-  { setAuthToken, setCurrentUser, logoutUser }
+  { setAuthToken, setCurrentUser, logoutUser, fetchNotPurchased }
 )(withRouter(Layout));
