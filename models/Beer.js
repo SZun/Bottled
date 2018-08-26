a2758194bc42c959074dd5d75a18229f6128b7ec
Sam G. Zun <zunsamuel97@gmail.com>
2018-08-26 01:00:50 -0500

    working on backend for reviews

---
+++ models/Beer.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const beerSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  image_url: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true,
+        trim: true
+      },
+      name: {
+        type: String,
+        required: true,
+        trim: true
+      }
+    }
+  ]
+});
+
+const Beer = mongoose.model('beerreview', beerSchema);
+
+export default Beer;
