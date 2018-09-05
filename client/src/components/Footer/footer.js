3c363969244b1753f8a0876a0aad93d7106c9187
Sam G. Zun <zunsamuel97@gmail.com>
2018-09-05 18:03:37 -0500

    footer text changed

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -4,7 +4,7 @@ import './footer.css';
 const FooterComponent = () => (
   <div className="z-depth-5 amber footer">
     <h5 style={{ fontWeight: 200 }}>
-      Copyright &copy; {new Date().getFullYear()} Bottled
+      Copyright Bottled&copy; {new Date().getFullYear()}
     </h5>
   </div>
 );
